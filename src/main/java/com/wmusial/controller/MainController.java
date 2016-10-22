070ec09dfb26189339dda3b2644ebe6b407155c9
Wojciech Musiał <wmusial@diib.com>
2016-10-22 08:48:34 +0200

    initial commit

---
+++ src/main/java/com/wmusial/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.wmusial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getMainPage() {
+
+        return "main";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+
+        return "login";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+
+        return "register";
+    }
+}
